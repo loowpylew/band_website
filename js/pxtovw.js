872b1b0947cbd537b43887fdd4d470657298af58
Lewis Taylor <theworksSE@outlook.com>
2021-09-13 20:25:02 +0100

    Further_changes

---
+++ js/pxtovw.js
@@ -0,0 +1,12 @@
+//1px = 100vw / viewport's width (in px)
+
+function convertPXToVW(px) {
+	return px * (100 / document.documentElement.clientWidth);
+}
+
+var all = document.getSelection('style.css') 
+
+elementList = parentNode.querySelectorAll();
+
+
+all.convertPXToVW()
